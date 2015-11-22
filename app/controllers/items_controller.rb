d0a1655679d44859ce87bd7857901a4e63afd204
横内 慧一 <leadop.yokouchi@gmail.com>
2015-11-22 12:35:19 +0900

    new index createの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,37 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+
   def show
     @item = Item.find(params[:id])
   end
+
+  def index
+  @items = Item.all
+  end
+
+    def create
+    @item = Item.new(item_params)
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  # params.require(key).permit(filter)
+  # key
+  #   Strong Parameters を適用したい params の key を指定する。
+  # filter
+  #   Strong Parameters で許可するカラムを指定する。
+
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
+
 
 end
